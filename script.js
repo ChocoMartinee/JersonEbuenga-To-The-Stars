276417d2f8349dba2cf35f23a69fe77292858b91
ChocoMartinee <jersonebuenga2@gmail.com>
2025-03-29 22:06:33 +0800

    added js

---
+++ script.js
@@ -0,0 +1,29 @@
+const descriptions = ["computer enthusiast", "game developer", "anime enjoyer"];
+let index = 0;
+let charIndex = 0;
+const typewriterElement = document.getElementById("typewriter");
+
+function typeEffect() {
+    if (charIndex < descriptions[index].length) {
+        typewriterElement.textContent += descriptions[index].charAt(charIndex);
+        charIndex++;
+        setTimeout(typeEffect, 100);
+    } else {
+        setTimeout(eraseEffect, 2000);
+    }
+}
+
+function eraseEffect() {
+    if (charIndex > 0) {
+        typewriterElement.textContent = descriptions[index].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(eraseEffect, 50);
+    } else {
+        index = (index + 1) % descriptions.length;
+        setTimeout(typeEffect, 500);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    typeEffect();
+});
